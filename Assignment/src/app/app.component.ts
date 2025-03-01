f9204eca61093be253a3104051c67e8cb7e042cc
unknown <amitkumar.vaghela@example.com>
2025-03-01 14:43:48 +0530

    initial commit

--- Assignment/src/app/app.component.ts
+++ Assignment/src/app/app.component.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  isNavActive = false;
-
-  toggleNav() {
-    this.isNavActive = !this.isNavActive;
-  }
+  title = 'Assignment';
 }
